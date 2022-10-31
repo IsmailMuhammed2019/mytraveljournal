6d8d7f5b57d731dfc14c751f9bd3f2556138a35b
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-10-31 12:28:37 +0100

    add places data

---
+++ src/data.js
@@ -0,0 +1,40 @@
+import { jSXNamespacedName } from "@babel/types";
+
+export default [
+  {
+    id: 1,
+    img: './imgs/mountfuji.jpg',
+    location: 'Japan',
+    Name: 'Mount Fuji',
+    link: 'https://en.wikipedia.org/wiki/Mount_Fuji',
+    date: '12 Jan, 2021 - 24 Jan, 2021',
+    info: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+  },
+  {
+    id: 2,
+    img: './imgs/sydneyoperahous.jpg',
+    location: 'Australia',
+    Name: 'Sydney Opera House',
+    link: 'https://en.wikipedia.org/wiki/Sydney_Opera_House',
+    date: '27 May, 2021 - 8 Jun, 2021',
+    info: "The Sydney Opera House is a multi-venue performing arts centre in Sydney.Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distrinctive buildings.",
+  },
+  {
+    id: 3,
+    img: './imgs/fiorde.jpg',
+    location: 'Norway',
+    Name: 'Geirangerfjord',
+    link: 'https://en.wikipedia.org/wiki/Geirangerfjord',
+    date: '01 Oct, 2021 - 18 Nov, 2021',
+    info: 'The Geiranger Fjord is a fjord in the Summer region of Maire Og Romsdal country. Normay. It is located in the Stranda Municipalty. .',
+  },
+  {
+    id: 4,
+    img: './imgs/hollywood.jpg',
+    location: 'Los Angeles',
+    Name: 'Hollywood',
+    link: 'https://en.wikipedia.org/wiki/Hollywood',
+    date: '01 Dec, 2021 - 18 Jan, 2022',
+    info: 'A larger-than-life symbol of the entertainment business, Hollywood beckons tourists with landmarks like TCL Chinese Theatre and star-studded Walk of Fame. ',
+  },
+]
