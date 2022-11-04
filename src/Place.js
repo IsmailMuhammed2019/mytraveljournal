fe06681957d313ba0eb5ffa863a3fd53a1272387
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-11-04 22:48:01 +0100

    add info

--- src/Place.js
+++ src/Place.js
@@ -10,7 +10,7 @@ const Place = (props) => {
         <p><img src='./imgs/location.png' alt='icon'/> {props.location} <a href='${props.link'>view on Google Maps</a></p>
         <h2>{props.name}</h2>
         <p className='date'>{props.date}</p>
-        <p>{props.text}</p>
+        <p>{props.info}</p>
       </div>
     </div>
   )
